eb7ab11c130f95e437a003bcec06a4af940195e0
Swapnil Sarwe <swapnilsarwe@gmail.com>
2021-09-13 10:06:24 +0530

    updated with the laravel shift rules

--- src/rules.php
+++ src/rules.php
@@ -1,36 +1,44 @@
 <?php
 return [
-    'phpdoc_indent' => true,
+    'array_syntax' => ['syntax' => 'short'],
     'binary_operator_spaces' => [
-        'operators' => ['=>' => null],
+        'default' => 'single_space',
+        'operators' => ['=>' => null]
     ],
     'blank_line_after_namespace' => true,
     'blank_line_after_opening_tag' => true,
-    'blank_line_before_statement' => true,
+    'blank_line_before_statement' => [
+        'statements' => ['return']
+    ],
     'braces' => true,
     'cast_spaces' => true,
+    'class_attributes_separation' => [
+        'elements' => ['method' => 'one']
+    ],
     'class_definition' => true,
-    'concat_space' => true,
+    'concat_space' => [
+        'spacing' => 'none'
+    ],
     'declare_equal_normalize' => true,
     'elseif' => true,
     'encoding' => true,
     'full_opening_tag' => true,
+    'fully_qualified_strict_types' => true, // added by Shift
     'function_declaration' => true,
     'function_typehint_space' => true,
-    'single_line_comment_style' => [
-        'comment_types' => ['hash'],
-    ],
     'heredoc_to_nowdoc' => true,
     'include' => true,
+    'increment_style' => ['style' => 'post'],
     'indentation_type' => true,
     'linebreak_after_opening_tag' => true,
+    'line_ending' => true,
     'lowercase_cast' => true,
-    // 'lowercase_constants' => true,
+    // 'lowercase_constants' => true, // deprecated in php cs fixer 3
     'lowercase_keywords' => true,
+    'lowercase_static_reference' => true, // added from Symfony
+    'magic_method_casing' => true, // added from Symfony
     'magic_constant_casing' => true,
     'method_argument_space' => true,
-    'class_attributes_separation' => true,
-    'visibility_required' => true,
     'native_function_casing' => true,
     'no_alias_functions' => true,
     'no_extra_blank_lines' => [
@@ -39,18 +47,22 @@ return [
             'throw',
             'use',
             'use_trait',
-        ],
+        ]
     ],
     'no_blank_lines_after_class_opening' => true,
     'no_blank_lines_after_phpdoc' => true,
     'no_closing_tag' => true,
     'no_empty_phpdoc' => true,
     'no_empty_statement' => true,
-    'no_extra_blank_lines' => true,
     'no_leading_import_slash' => true,
     'no_leading_namespace_whitespace' => true,
+    'no_mixed_echo_print' => [
+        'use' => 'echo'
+    ],
     'no_multiline_whitespace_around_double_arrow' => true,
-    'multiline_whitespace_before_semicolons' => true,
+    'multiline_whitespace_before_semicolons' => [
+        'strategy' => 'no_multi_line'
+    ],
     'no_short_bool_cast' => true,
     'no_singleline_whitespace_before_semicolons' => true,
     'no_spaces_after_function_name' => true,
@@ -68,44 +80,42 @@ return [
     'normalize_index_brace' => true,
     'not_operator_with_successor_space' => true,
     'object_operator_without_whitespace' => true,
-    'ordered_imports' => ['sort_algorithm' => 'length'],
+    'ordered_imports' => ['sort_algorithm' => 'alpha'],
     'phpdoc_indent' => true,
-    // 'phpdoc_inline_tag' => true,
+    // 'phpdoc_inline_tag' => true, // deprecated in php cs fixer 3
     'phpdoc_no_access' => true,
     'phpdoc_no_package' => true,
     'phpdoc_no_useless_inheritdoc' => true,
     'phpdoc_scalar' => true,
     'phpdoc_single_line_var_spacing' => true,
-    'phpdoc_summary' => true,
-    'phpdoc_to_comment' => true,
+    'phpdoc_summary' => false,
+    'phpdoc_to_comment' => false, // override to preserve user preference
     'phpdoc_trim' => true,
     'phpdoc_types' => true,
     'phpdoc_var_without_name' => true,
-    'increment_style' => ['style' => 'post'],
-    'no_mixed_echo_print' => true,
-    'no_unused_imports' => true,
-    // 'psr4' => true,
+    // 'psr4' => true, // deprecated in php cs fixer 3
     'self_accessor' => true,
-    'array_syntax' => ['syntax' => 'short'],
     'short_scalar_cast' => true,
-    'simplified_null_return' => true,
+    'simplified_null_return' => false, // disabled as "risky"
     'single_blank_line_at_eof' => true,
     'single_blank_line_before_namespace' => true,
     'single_class_element_per_statement' => true,
     'single_import_per_statement' => true,
     'single_line_after_imports' => true,
+    'single_line_comment_style' => [
+        'comment_types' => ['hash']
+    ],
     'single_quote' => true,
     'space_after_semicolon' => true,
     'standardize_not_equals' => true,
     'switch_case_semicolon_to_colon' => true,
     'switch_case_space' => true,
     'ternary_operator_spaces' => true,
-    // 'trailing_comma_in_multiline_array' => true,
+    'trailing_comma_in_multiline' => true,
     'trim_array_spaces' => true,
     'unary_operator_spaces' => true,
-    'line_ending' => true,
+    'visibility_required' => [
+        'elements' => ['method', 'property']
+    ],
     'whitespace_after_comma_in_array' => true,
-    'lowercase_static_reference' => true, // added from Symfony
-    'magic_method_casing' => true, // added from Symfony
-    'fully_qualified_strict_types' => true, // added
 ];
